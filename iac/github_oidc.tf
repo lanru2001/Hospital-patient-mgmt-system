8ce89be7c9a2d79ff947974990ce8dd33b655c87
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2026-08-20 11:44:51 -0500

    Update github_oidc.tf

--- iac/github_oidc.tf
+++ iac/github_oidc.tf
@@ -1,77 +1,77 @@
 # One-time setup: lets GitHub Actions assume an AWS role via OIDC, no stored
 # access keys. Update `repo` below to "org/repo".
 
-variable "github_repo" {
-  type    = string
-  default = "your-org/hospital-pms"
-}
+# variable "github_repo" {
+#   type    = string
+#   default = "your-org/hospital-pms"
+# }
 
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
+# resource "aws_iam_openid_connect_provider" "github" {
+#   url             = "https://token.actions.githubusercontent.com"
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+# }
 
-data "aws_iam_policy_document" "github_assume" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
-    }
-  }
-}
+# data "aws_iam_policy_document" "github_assume" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     principals {
+#       type        = "Federated"
+#       identifiers = [aws_iam_openid_connect_provider.github.arn]
+#     }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "token.actions.githubusercontent.com:aud"
+#       values   = ["sts.amazonaws.com"]
+#     }
+#     condition {
+#       test     = "StringLike"
+#       variable = "token.actions.githubusercontent.com:sub"
+#       values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "github_deploy" {
-  name               = "${var.project}-github-deploy"
-  assume_role_policy = data.aws_iam_policy_document.github_assume.json
-}
+# resource "aws_iam_role" "github_deploy" {
+#   name               = "${var.project}-github-deploy"
+#   assume_role_policy = data.aws_iam_policy_document.github_assume.json
+# }
 
-resource "aws_iam_role_policy" "github_deploy" {
-  name = "${var.project}-github-deploy-policy"
-  role = aws_iam_role.github_deploy.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:PutImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload",
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecs:DescribeTaskDefinition",
-          "ecs:RegisterTaskDefinition",
-          "ecs:UpdateService",
-          "ecs:DescribeServices",
-        ]
-        Resource = "*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = ["iam:PassRole"]
-        Resource = [aws_iam_role.execution.arn, aws_iam_role.task.arn]
-      }
-    ]
-  })
-}
+# resource "aws_iam_role_policy" "github_deploy" {
+#   name = "${var.project}-github-deploy-policy"
+#   role = aws_iam_role.github_deploy.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ecr:GetAuthorizationToken",
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:BatchGetImage",
+#           "ecr:PutImage",
+#           "ecr:InitiateLayerUpload",
+#           "ecr:UploadLayerPart",
+#           "ecr:CompleteLayerUpload",
+#         ]
+#         Resource = "*"
+#       },
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ecs:DescribeTaskDefinition",
+#           "ecs:RegisterTaskDefinition",
+#           "ecs:UpdateService",
+#           "ecs:DescribeServices",
+#         ]
+#         Resource = "*"
+#       },
+#       {
+#         Effect   = "Allow"
+#         Action   = ["iam:PassRole"]
+#         Resource = [aws_iam_role.execution.arn, aws_iam_role.task.arn]
+#       }
+#     ]
+#   })
+# }
